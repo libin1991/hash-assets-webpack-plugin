a0851ce336e8066480bbee68a40877161379537f
xsbear <xsbear@gmail.com>
2017-05-10 23:06:24 +0800

    Support no hash generate mode.
    Update to Version 0.3.0
    This mode is used in test env just extract resources from dynamic template without hash.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var extractAssets = require('./lib/extractAssets');
 function HashAssetsPlugin (options) {
   this.options = merge({}, {
     path: '.',
+    hash: true,
     filename: 'assets-hash.json',
     keyTemplate: 'js/[name].js',
     hashLength: 20,
@@ -44,45 +45,51 @@ HashAssetsPlugin.prototype = {
 
     compiler.plugin('after-emit', function (compilation, callback) {
 
-      var stats = compilation.getStats().toJson({
-        hash: false,
-        publicPath: false,
-        assets: false,
-        chunks: true,
-        modules: false,
-        source: false,
-        errorDetails: false,
-        timings: false
-      });
+      if(options.hash) {
+        var stats = compilation.getStats().toJson({
+          hash: false,
+          publicPath: false,
+          assets: false,
+          chunks: true,
+          modules: false,
+          source: false,
+          errorDetails: false,
+          timings: false
+        });
 
-      var chunks = stats.chunks;
+        var chunks = stats.chunks;
 
-      var hashMap = {};
-      chunks.forEach(function (chunk) {
-        if(chunk.initial && chunk.names.length){
-          chunk.files.forEach(function (file) {
-            var key;
-            if(typeof options.keyTemplate === 'string'){
-              key = options.keyTemplate.replace('[name]', chunk.names[0]);
-            } else if(typeof options.keyTemplate === 'function') {
-              key = options.keyTemplate(file);
-            }
-            hashMap[key] = chunk.hash.substr(0, options.hashLength);
-          });
-        }
-      });
+        var hashMap = {};
+        chunks.forEach(function (chunk) {
+          if(chunk.initial && chunk.names.length){
+            chunk.files.forEach(function (file) {
+              var key;
+              if(typeof options.keyTemplate === 'string'){
+                key = options.keyTemplate.replace('[name]', chunk.names[0]);
+              } else if(typeof options.keyTemplate === 'function') {
+                key = options.keyTemplate(file);
+              }
+              hashMap[key] = chunk.hash.substr(0, options.hashLength);
+            });
+          }
+        });
 
-      callback();
+        callback();
 
-      var assetsHash = extractAssets(options);
+        var assetsHash = extractAssets(options);
 
-      Object.assign(hashMap, assetsHash);
-      var output = JSON.stringify(hashMap, 'null', options.prettyPrint ? '\t' : '');
-      var outputPath = path.join(options.path, options.filename);
-      utils.mkdir(path.dirname(outputPath));
-      fs.writeFileSync(outputPath, output);
+        Object.assign(hashMap, assetsHash);
+        var output = JSON.stringify(hashMap, 'null', options.prettyPrint ? '\t' : '');
+        var outputPath = path.join(options.path, options.filename);
+        utils.mkdir(path.dirname(outputPath));
+        fs.writeFileSync(outputPath, output);
 
-      process.stdout.write('Assets Hash Generated: ' + options.filename.info + '\n');
+        process.stdout.write('Assets Hash Generated: ' + options.filename.info + '\n');
+
+      } else {
+        callback();
+        extractAssets(options);
+      }
     });
   }
 };
